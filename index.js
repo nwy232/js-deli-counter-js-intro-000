f835354b896abb8dd6f0312a19ff9cf313f28240
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-05 00:44:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,14 +21,13 @@ function nowServing(katzDeliLine)
 
 function currentLine(katzDeliLine)
 {
-  var i=0
   if(katzDeliLine.length==0)
   {
     return 'The line is currently empty.'
   }
 
   else {
-    return `The line is currently: ${[i++]katzDeliLine}`
+    return `The line is currently: ${katzDeliLine[0,katzDeliLine.length]}`
     /*console.log('The line is currently: ')
     for(var i=0;i<katzDeliLine.length;i++)
     {
