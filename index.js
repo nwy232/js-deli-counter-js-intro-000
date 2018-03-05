a52159e6c05205601748265906af3da76c9ad729
Kate Schwarz <kateschwarz0010@gmail.com>
2018-03-05 00:24:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var katzDeli=[];
 
 function takeANumber(katzDeliLine,name)
 {
-  katzDeli=[name,...katzDeliLine]
-  return `Welcome, ${name}. You are number ${katzDeliLine.length+1} in line.`
+  katzDeliLine.push(name)
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
 function nowServing()
